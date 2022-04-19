97ef37aec6010dacf22241dea972a806e0b24d37
killian.mannarelli <killian.mannarelli@student.jade-hs.de>
2022-04-19 13:09:24 +0000

    Setup one server

---
+++ CAxBooking_django_react/api/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from rest_framework import generics, status
+from .serializers import TodoSerializer
+
+from .models import Todo
+# Create your views here.
+
+
+class TodoListView(generics.ListAPIView):
+    model = Todo
+    serializer_class = TodoSerializer
+    
