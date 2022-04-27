e4387b3dce983923ae751ba6cbf22513848d1b3a
gabin.charasson <gabin.charasson@student.jade-hs.de>
2022-04-27 12:21:20 +0200

    no improvement yet

--- CAxBooking_django_react/frontend/src/components/MainPageComponent/BookingComponent/Booking.js
+++ CAxBooking_django_react/frontend/src/components/MainPageComponent/BookingComponent/Booking.js
@@ -1,12 +1,13 @@
-
 import React from 'react';
 
 export default function Booking(props) {
 
-    let [computerName, setComputerName] = React.useState(fetchComputer);
-    let [computerRoom, setComputerRoom] = React.useState(fetchComputer);
-    let [roomName, setRoomName] = React.useState(fetchRoom());
+    const [computerName, setComputerName] = React.useState();
+    const [computerRoom, setComputerRoom] = React.useState();
 
+    const [roomName, setRoomName] = React.useState();
+
+    fetchComputer()
 
     function fetchComputer() {
         fetch("http://127.0.0.1:8000/api/computers/search?computer_id=" + props.computer, {
@@ -16,39 +17,23 @@ export default function Booking(props) {
         }).then(function (data) {
             let list = JSON.parse(data);
             setComputerName(list[0].name);
-            setComputerRoom(list[0].room);
-            waitForElement()
+            setComputerRoom(list[0].room,fetchRoom());
+        });
+    }
 
+    function fetchRoom() {
+        fetch("http://127.0.0.1:8000/api/rooms/search?room_id=" + computerRoom, {
+            method: "GET"
+        }).then(function (response) {
+            return response;
+        }).then(function (data) {
+            let list = JSON.parse(data);
+            setRoomName(list[0].name);
         }.bind(this));
     }
-
-    function waitForElement() {
-        if (typeof computerRoom !== "undefined") {
-            console.log("bbbb", computerRoom)
-            return;
-        }
-        else {
-            setTimeout(waitForElement, 250);
-        }
-    }
-
-    function fetchRoom() {/*  */
-        console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", computerRoom)
-        if (typeof computerRoom !== "undefined") {
-            fetch("http://127.0.0.1:8000/api/rooms/search?room_id=" + computerRoom, {
-                method: "GET"
-            }).then(function (response) {
-                return response;
-            }).then(function (data) {
-                let list = JSON.parse(data);
-                setRoomName(list[0].name);
-            }.bind(this));
-        } else {
-        }
-    }
-
 
     return (
         <p>{computerName} - {roomName} - hh:mm - dd/MM</p>
     )
 }
+
