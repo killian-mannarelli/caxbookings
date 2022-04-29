6f5a83c65c33c2b45bfd2e67d9e76c9a1bda1f93
mannarelli-killian <killian.mannarelli@outlook.fr>
2022-04-29 10:17:01 +0200

    Reloading after changing the date

--- CAxBooking_django_react/frontend/src/components/RoomLayoutComponent/RoomLayoutComponent.js
+++ CAxBooking_django_react/frontend/src/components/RoomLayoutComponent/RoomLayoutComponent.js
@@ -1,102 +1,128 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import "./roomlayout.css";
 import Header from "../MainPageComponent/Header";
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import PcComponent from "./PcComponent";
 import Container from '@mui/material/Container';
+import TimeSpan from "../MainPageComponent/TimeSpanComponent/TimeSpan";
+import Axios from "axios";
 
-export default class RoomLayout extends Component {
+export default function RoomLayout(props) {
 
-  rooms = [];
+  const [computers, setComputers] = React.useState([]);
+  const [urlInfos, setUrlInfos] = React.useState(null);
+  const [roomName, setRoomName] = React.useState("");
 
-  constructor(props) {
-    super(props);
-    this.state = { computers: [], roomId : 1, roomName : "Room 1" };
-    
-    
-    
+  useEffect(() => {
+    scrapUrl();
 
+
+  }, []
+
+  );
+
+  useEffect(() => {
+    if (urlInfos == null) return;
+    fetchApi();
+    fetchToGetRoom();
+  }, [urlInfos]);
+
+  
+  const callBackFromTimeSpan = (day, start, end) => {
+
+    let StartDate = new Date(day.toISOString());
+    StartDate.setHours(start.getHours());
+    StartDate.setMinutes(start.getMinutes());
+    StartDate.setSeconds(start.getSeconds());
+    let EndDate = new Date(day.toISOString());
+    EndDate.setHours(end.getHours());
+    EndDate.setMinutes(end.getMinutes());
+    EndDate.setSeconds(end.getSeconds());
+
+
+   let newUrlInfos = {
+      roomId : urlInfos.roomId,
+      startTime : StartDate,
+      endTime : EndDate,
+      day : day
+    }
+    setUrlInfos(newUrlInfos);
+  }
+  const fetchApi = () => {
+
+    let startStringIso = urlInfos.startTime.toISOString();
+    let endStringIso = urlInfos.endTime.toISOString();
+    Axios.get("http://127.0.0.1:8000/api/computerinroom?room_id=" + urlInfos.roomId + "&time_span_start=" + startStringIso + "&time_span_end=" + endStringIso).then(res => {
+      setComputers(res.data);
+      //console.log(computers);
+    });
+  }
+
+  const fetchToGetRoom = () => {
+    //fetch the room name
+    fetch("http://127.0.0.1:8000/api/rooms/search?room_id=" + urlInfos.roomId, {
+      method: "GET"
+    }).then(function (response) {
+      return response.text();
+    }).then(function (data) {
+      var list = JSON.parse(data);
+      setRoomName(list[0].name);
+    });
 
   }
 
+  const scrapUrl =  () => {
+    let url = window.location.href;
 
-  render() {
-    return (
+    let newUrlInfos = {
+      roomId: url.split("room_id=")[1].split("&")[0],
+      startTime: new Date(url.split("start=")[1].split("&")[0]),
+      endTime: new Date(url.split("stop=")[1].split("&")[0]),
+      day: new Date(url.split("start=")[1].split("&")[0])
 
-      <div >
-        <Header />
-        <p>{this.state.roomName}</p>
-        <Container>
+    }
+    setUrlInfos(newUrlInfos);
+  }
+
+  return (
+
+    <div >
+
+      <Header />
+      <p>{roomName}</p>
+      <TimeSpan
+        day={urlInfos?.day}
+        start={urlInfos?.startTime}
+        end={urlInfos?.endTime}
+        callback={callBackFromTimeSpan}
+      />
+
+      <Container>
         <Box sx={{
           border: '1px solid #595850',
           borderRadius: '40px',
         }}>
 
-        <Grid container spacing={3} wrap="wrap" direction = "row" alignItems ="center">
-          {this.state.computers.map((pc) =>
-              <PcComponent pc={pc}  />
-          )}
+          <Grid container spacing={3} wrap="wrap" direction="row" alignItems="center">
+            {computers.map((pc) =>
+              <PcComponent pc={pc} />
+            )}
 
           </Grid>
-          </Box>
-          </Container>
-      </div>
-    );
-  };
+        </Box>
+      </Container>
+    </div>
+  );
+
+  
 
   
 
 
-  fetchApi() {
-    let url = window.location.href;
-    let roomIdd = url.split("room_id=")[1].split("&")[0];
-    let startTime = url.split("start=")[1].split("&")[0];
-    let endTime = url.split("stop=")[1].split("&")[0];
-    if(roomIdd === undefined || startTime === undefined || endTime === undefined){
-      roomIdd = 1;
-      startTime = "2002-07-13T16:30:00Z";
-      endTime = "2002-07-13T17:00:00Z";
-    };
 
-   
 
-    fetch("http://127.0.0.1:8000/api/computerinroom?room_id="+roomIdd +"&time_span_start=" + startTime + "&time_span_end=" + endTime, {
-      method: "GET"
-    }).then(function (response) {
-      return response.text();
-    }).then(function (data) {
-      var list = JSON.parse(data);
-      
-      this.setState({ computers: list });
-      this.setState({roomId : roomIdd});
-      this.fetchToGetRoom();
-    }.bind(this));
-  }
 
-  fetchToGetRoom() {
-    
-    //take the room id from the state
-    let roomId = this.state.roomId;
-    //fetch the room name
-    fetch("http://127.0.0.1:8000/api/rooms/search?room_id="+roomId, {
-      method: "GET"
-    }).then(function (response) {
-      return response.text();
-    }).then(function (data) {
-      var list = JSON.parse(data);
-      this.setState({ roomName: list[0].name });
-    }.bind(this));
-  }
-
-  componentDidMount() {
-this.fetchApi();
-
-    
-      
-    
-    
-  }
 }
 
 
