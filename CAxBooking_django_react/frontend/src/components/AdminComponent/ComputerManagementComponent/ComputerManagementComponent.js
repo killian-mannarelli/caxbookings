2718c8992b2a9d442a73bfa622dc857d3899f740
gabin.charasson <gabin.charasson@student.jade-hs.de>
2022-05-06 16:55:31 +0200

    admin components created

---
+++ CAxBooking_django_react/frontend/src/components/AdminComponent/ComputerManagementComponent/ComputerManagementComponent.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function ComputerManagement(){
+    return (
+        <div id="AccountManagement">
+ssssssssssssssssssssssssssssss
+        </div>
+    )
+}
--- CAxBooking_django_react/frontend/src/components/AdminComponent/SelectionComponent/SelectionComponent.js
+++ CAxBooking_django_react/frontend/src/components/AdminComponent/SelectionComponent/SelectionComponent.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
-import RoomManagementComponent from '../RoomManagementComponent/RoomManagementComponent';
+import AccountManagement from '../AccountManagementComponent/AccountManagementComponent';
+import ComputerManagement from '../ComputerManagementComponent/ComputerManagementComponent';
+import RoomManagement from '../RoomManagementComponent/RoomManagementComponent';
 import Stats from '../StatsComponent/StatsComponent';
 
 export default function Selection(props) {
@@ -13,17 +15,21 @@ export default function Selection(props) {
             </div>
 
             <div onClick={() => {
-                props.setContent(<RoomManagementComponent/>)
+                props.setContent(<RoomManagement/>)
             }}>
-                <button className="login-logout CAxButton">Stats</button>
+                <button className="login-logout CAxButton">Rooms</button>
             </div>
 
-            <div>
-                <button className="login-logout CAxButton">Stats</button>
+            <div onClick={() => {
+                props.setContent(<ComputerManagement/>)
+            }}>
+                <button className="login-logout CAxButton">Computers</button>
             </div>
 
-            <div>
-                <button className="login-logout CAxButton">Stats</button>
+            <div onClick={() => {
+                props.setContent(<AccountManagement/>)
+            }}>
+                <button className="login-logout CAxButton">Users</button>
             </div>
         </div>
     )
