2762063ca3fba572e4122a2b8785fb4113d9977d
gabin.charasson <gabin.charasson@student.jade-hs.de>
2022-05-06 16:46:19 +0200

    rebasing ?

--- CAxBooking_django_react/frontend/src/components/AdminComponent/SelectionComponent/SelectionComponent.js
+++ CAxBooking_django_react/frontend/src/components/AdminComponent/SelectionComponent/SelectionComponent.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import RoomManagementComponent from '../RoomManagementComponent/RoomManagementComponent';
 import Stats from '../StatsComponent/StatsComponent';
 
 export default function Selection(props) {
@@ -12,7 +13,7 @@ export default function Selection(props) {
             </div>
 
             <div onClick={() => {
-                props.setContent(<p>aaaaaaaaaaaaaabs</p>)
+                props.setContent(<RoomManagementComponent/>)
             }}>
                 <button className="login-logout CAxButton">Stats</button>
             </div>
