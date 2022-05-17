ed15e5d2c76ef35b526c2c895b1fad85f1c7ac85
gabin.charasson <gabin.charasson@student.jade-hs.de>
2022-05-17 15:26:02 +0200

    css tweaks

--- CAxBooking_django_react/frontend/src/components/MainPageComponent/RoomDisplayComponent/RoomDisplay.js
+++ CAxBooking_django_react/frontend/src/components/MainPageComponent/RoomDisplayComponent/RoomDisplay.js
@@ -7,8 +7,8 @@ export default function RoomDisplay(props) {
     let realtimestart = new Date(props.start);
     let realtimeend = new Date(props.end)
     //add 2 hours to both
-    realtimestart.setHours(realtimestart.getHours() );
-    realtimeend.setHours(realtimeend.getHours() );
+    realtimestart.setHours(realtimestart.getHours());
+    realtimeend.setHours(realtimeend.getHours());
     //reconvert back in to string
     let start = realtimestart.toISOString();
     let end = realtimeend.toISOString();
@@ -22,28 +22,21 @@ export default function RoomDisplay(props) {
                 <thead>
                     <tr>
                         <th>Room n°</th>
-                        <th>Available From-To</th>
                         <th>Disponibility</th>
-                        <th>---</th>
                         <th>---</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {props.rooms.map((val,key) => {
+                    {props.rooms.map((val, key) => {
                         return (
-                            <tr key = {key}>
+                            <tr key={key}>
                                 <td>{val?.room_name ?? "Placeholder"}</td>
-                                <td>8-12</td>
                                 <td>{val?.room_current_capacity ?? 0}/{val?.room_capacity ?? 0}</td>
                                 <td>
-                                    <a href="">
-                                        <button className="stats-button CAxButton">Room stats</button>
-                                    </a>
-                                </td>
-                                <td>
-                                    <a href={"http://127.0.0.1:8000/room/room_id="+val.room_id+"&start="+start+"&stop="+end}>
-                                        <button className="checkout-button CAxButton">Checkout</button>
-                                    </a>
+                                    <button className="checkout-button CAxButton" onClick={() => {
+                                        window.location.replace("http://127.0.0.1:8000/room/room_id=" + val.room_id + "&start=" + start + "&stop=" + end);
+                                    }}>Checkout</button>
+
                                 </td>
                             </tr>
                         )
